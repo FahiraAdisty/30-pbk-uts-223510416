4793a4e35aea764811a300622ab9bd8d51cf5cc0
FahiraAdisty <fahiraadisty07@gmail.com>
2024-04-28 22:37:17 +0700

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,123 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <h2>Daftar Kegiatan</h2>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <input type="text" v-model="newKegiatan" placeholder="Masukkan kegiatan baru" />
+    <button @click="tambahKegiatan">Add</button>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+    <ul>
+      <li v-for="(kegiatan, index) in kegiatanList" :key="index">
+        <input
+          type="checkbox"
+          v-model="kegiatanList[index].selesai"
+          @change="ceklistKegiatan(index)"
+        />
+        <span class="kegiatan" :class="{ selesai: kegiatan.selesai }">{{ kegiatan.nama }}</span>
+        <button @click="batalkanKegiatan(index)">Cancel</button>
+      </li>
+    </ul>
 
-  <RouterView />
+    <button @click="tampilkanBelumSelesai">Tampilkan List yang Belum Selesai</button>
+  </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+interface Kegiatan {
+  nama: string
+  selesai: boolean
+}
+
+export default defineComponent({
+  name: 'App',
+
+  data() {
+    return {
+      kegiatanList: [
+        { nama: 'Belajar TypeScript', selesai: false },
+        { nama: 'Menyelesaikan tugas', selesai: false },
+        { nama: 'Membeli bahan makanan', selesai: true }
+      ] as Kegiatan[],
+      newKegiatan: ''
+    }
+  },
+
+  methods: {
+    tambahKegiatan() {
+      if (this.newKegiatan) {
+        this.kegiatanList.push({ nama: this.newKegiatan, selesai: false })
+        this.newKegiatan = ''
+      }
+    },
+    ceklistKegiatan(index: number) {
+      this.kegiatanList[index].selesai = !this.kegiatanList[index].selesai
+    },
+    batalkanKegiatan(index: number) {
+      this.kegiatanList.splice(index, 1)
+    },
+    tampilkanBelumSelesai() {
+      this.kegiatanList = this.kegiatanList.filter((kegiatan) => !kegiatan.selesai)
+    }
+  }
+})
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+/* style.css */
+body {
+  font-family: sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+#app {
+  margin: 20px;
+  padding: 20px;
+  border: 1px solid #fd007e;
+  border-radius: 5px;
+  width: 300px;
 }
 
-nav {
+input[type='text'] {
   width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+  padding: 10px;
+  margin-bottom: 10px;
+  border: 1px solid #fd007e;
+  border-radius: 5px;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+button {
+  padding: 5px;
+  margin-bottom: 5%;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  background-color: #da139e;
+  color: #fff;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+ul {
+  list-style: none;
+  padding: 0;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+li {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
 }
 
-nav a:first-of-type {
-  border: 0;
+input[type='checkbox'] {
+  margin-right: 10px;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.kegiatan {
+  padding: 5px;
+  border: 1px solid #fd007e;
+  border-radius: 5px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.selesai {
+  text-decoration: line-through;
+  color: #888;
 }
 </style>
